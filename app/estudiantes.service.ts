9e0574570e3523dce11f88dc1bb40b0f9da3dbc6
John Pol <jap.jhon2@gmail.com>
2017-03-16 15:30:33 -0500

    Add files via upload

--- app/estudiantes.service.ts
+++ app/estudiantes.service.ts
@@ -1,11 +1,9 @@
 export class EstudiantesService{
   listaDeEstudiantes(universidad: any): Array<string> {
-if (universidad = 'universidad nacional') {
+if (universidad = 'universidad naciona') {
     return ['Oscar Munera.', 'Simon Lopez.', 'Carmenza Zapata.', 'Olivia Morales.', 'Orfa Linares.'];
-    /*return ['Oscar Munera', 'Simon Lopez', 'Rogelio Nuñes'];*/
   }else {
-   return ['Natalia Ochoa', 'Marina Torres', 'Luis Severich'];
-  /*return ['Natalia Ochoa', 'Marina Torres', 'Luis Severich'];*/
+   return ['Natalia Ochoa'];
     }
   }
 }
